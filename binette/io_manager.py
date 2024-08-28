99fafaa517173f45ec7b5c305a6a78a5956ab1c0
JeanMainguy <jean.mainguy@outlook.fr>
2024-08-29 00:45:09 +0200

    improve fct to infer bin set name

--- binette/io_manager.py
+++ binette/io_manager.py
@@ -1,36 +1,97 @@
 import logging
-import os
 import pyfastx
-from typing import Iterable, List, Dict
+from typing import Iterable, List, Dict, Tuple
 import csv
 
 from binette.bin_manager import Bin
 
+from pathlib import Path
 
-def infer_bin_name_from_bin_inputs(input_bins: List[str]) -> Dict[str, str]:
+def get_paths_common_prefix_suffix(paths: List[Path]) -> Tuple[List[str], List[str], List[str]]:
     """
-    Infer bin names from a list of bin input directories.
+    Determine the common prefix parts, suffix parts, and common extensions of the last part of a list of pathlib.Path objects.
 
-    :param input_bins: List of input bin directories.
-    :return: Dictionary mapping inferred bin names to their corresponding directories.
+    :param paths: List of pathlib.Path objects.
+    :return: A tuple containing three lists:
+             - The common prefix parts.
+             - The common suffix parts.
+             - The common extensions of the last part of the paths.
     """
-    logging.debug("Inferring bin names from input bins:")
+    # Extract parts for all paths
+    parts = [list(path.parts) for path in paths]
+    
+    # Find the common prefix
+    if not parts:
+        return [], [], []
+    
+    # Initialize common prefix and suffix lists
+    common_prefix = list(parts[0])
+    common_suffix = list(parts[0])
+    # Determine common prefix
+    for part_tuple in parts[1:]:
+        common_prefix_length = min(len(common_prefix), len(part_tuple))
+        common_prefix = [common_prefix[i] for i in range(common_prefix_length) if common_prefix[:i+1] == part_tuple[:i+1]]
+        if not common_prefix:
+            break
 
-    commonprefix_len = len(os.path.commonprefix(input_bins))
-    reversed_strings = [s[::-1] for s in input_bins]
-    commonsufix_len = len(os.path.commonprefix(reversed_strings))
+    # Determine common suffix
+    for part_tuple in parts[1:]:
+        common_suffix_length = min(len(common_suffix), len(part_tuple))
+        common_suffix = [common_suffix[-i] for i in range(1, common_suffix_length + 1) if common_suffix[-i:] == part_tuple[-i:]]
+        if not common_suffix:
+            break
+    if len(parts) > 1:
+        common_suffix.reverse()
 
-    bin_name_to_bin_dir = {d[commonprefix_len: len(d) - commonsufix_len]: d for d in input_bins}
+    # Determine common extensions of the last part of the paths
+    if len(paths) == 1:
+        common_extensions = paths[0].suffixes
+    else:
+        common_extensions = list(paths[0].suffixes)
+        for path in paths[1:]:
+            common_extension_length = min(len(common_extensions), len(path.suffixes))
+            common_extensions = [common_extensions[i] for i in range(common_extension_length) if common_extensions[i] == path.suffixes[i]]
+            if not common_extensions:
+                break
+    
+    return common_prefix, common_suffix, common_extensions
+    
+def infer_bin_set_names_from_input_paths(input_bins: List[Path]) -> Dict[str, Path]:
+    """
+    Infer bin set names from a list of bin input directories or files.
 
-    logging.debug(f"Input bins:  {' '.join(input_bins)}")
-    logging.debug(f"Common prefix to remove: {os.path.commonprefix(reversed_strings)[::-1]}")
-    logging.debug(f"Common suffix to remove: {os.path.commonprefix(input_bins)}")
+    :param input_bins: List of input bin directories or files.
+    :return: Dictionary mapping inferred bin names to their corresponding directories or files.
+    """
+    bin_name_to_bin_dir = {}
+
+    common_prefix, common_suffix, common_extensions = get_paths_common_prefix_suffix(input_bins)
+    print(common_prefix, common_suffix, common_extensions )
+    for path in input_bins:
+
+        specific_parts = path.parts[len(common_prefix):len(path.parts)-len(common_suffix)]
+
+        if not common_suffix and common_extensions:
+            last_specific_part = specific_parts[-1].split('.')[:-len(common_extensions)] 
+            specific_parts = list(specific_parts[:-1]) + last_specific_part
+
+
+        bin_set_name = '/'.join(specific_parts)
+        if bin_set_name == "":
+            bin_set_name = path.as_posix()
+
+        bin_name_to_bin_dir[bin_set_name] = path
+
+    logging.debug(f"Input bins: {' '.join([path.as_posix() for path in input_bins])}")
+    logging.debug(f"Common prefix to remove: {common_prefix}")
+    logging.debug(f"Common suffix to remove: {common_suffix}")
+    logging.debug(f"Common extension to remove: {common_suffix}")
     logging.debug(f"bin_name_to_bin_dir: {bin_name_to_bin_dir}")
 
     return bin_name_to_bin_dir
 
 
-def write_bin_info(bins: Iterable[Bin], output: str, add_contigs: bool = False):
+def write_bin_info(bins: Iterable[Bin], output: Path, add_contigs: bool = False):
     """
     Write bin information to a TSV file.
 
@@ -67,7 +128,7 @@ def write_bin_info(bins: Iterable[Bin], output: str, add_contigs: bool = False):
         writer.writerows(bin_infos)
 
 
-def write_bins_fasta(selected_bins: List[Bin], contigs_fasta: str, outdir: str):
+def write_bins_fasta(selected_bins: List[Bin], contigs_fasta: Path, outdir: Path):
     """
     Write selected bins' contigs to separate FASTA files.
 
@@ -76,10 +137,10 @@ def write_bins_fasta(selected_bins: List[Bin], contigs_fasta: str, outdir: str):
     :param outdir: Output directory to save the individual bin FASTA files.
     """
 
-    fa = pyfastx.Fasta(contigs_fasta, build_index=True)
+    fa = pyfastx.Fasta(contigs_fasta.as_posix(), build_index=True)
 
     for sbin in selected_bins:
-        outfile = os.path.join(outdir, f"bin_{sbin.id}.fa")
+        outfile = outdir / f"bin_{sbin.id}.fa"
 
         with open(outfile, "w") as outfl:
             sequences = (f">{c}\n{fa[c]}" for c in sbin.contigs)
@@ -111,7 +172,7 @@ def check_contig_consistency(contigs_from_assembly: Iterable[str],
     assert are_contigs_consistent, message
 
 
-def check_resume_file(faa_file: str, diamond_result_file: str) -> None:
+def check_resume_file(faa_file: Path, diamond_result_file: Path) -> None:
     """
     Check the existence of files required for resuming the process.
 
@@ -120,15 +181,15 @@ def check_resume_file(faa_file: str, diamond_result_file: str) -> None:
     :raises FileNotFoundError: If the required files don't exist for resuming.
     """
 
-    if os.path.isfile(faa_file) and os.path.isfile(diamond_result_file):
+    if faa_file.exists() and diamond_result_file.exists():
         return
 
-    if not os.path.isfile(faa_file):
+    if not faa_file.exists():
         error_msg = f"Protein file '{faa_file}' does not exist. Resuming is not possible."
         logging.error(error_msg)
         raise FileNotFoundError(error_msg)
 
-    if not os.path.isfile(diamond_result_file):
+    if not diamond_result_file.exists():
         error_msg = f"Diamond result file '{diamond_result_file}' does not exist. Resuming is not possible."
         logging.error(error_msg)
         raise FileNotFoundError(error_msg)
