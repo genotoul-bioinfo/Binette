35b683c87c5eaf4b6d9781d8d840db26a1fa0e6e
JeanMainguy <jean.mainguy@outlook.fr>
2025-10-06 20:08:00 +0200

    add more fct tests

--- tests/functional_tests/test_binette_command.py
+++ tests/functional_tests/test_binette_command.py
@@ -41,6 +41,15 @@ def test_wrong_input(tmp_path):
     )
     assert result.exit_code == 1
 
+def compare_results(result_file: Path, expected_file: Path):
+    content_actual = result_file.read_text()
+    content_expected = expected_file.read_text()
+    print(content_actual)
+    assert content_actual == content_expected, (
+        f"Content mismatch for {result_file}. "
+        f"Expected content from {expected_file}."
+    )
+
 
 @pytest.mark.requires_test_data
 def test_bin_dir_input(test_data_path: Path, tmp_path):
@@ -69,11 +78,13 @@ def test_bin_dir_input(test_data_path: Path, tmp_path):
     print(result.stderr)
     assert result.exit_code == 0
 
-
+    result_table = tmp_path / "test_results_from_dirs" / "final_bins_quality_reports.tsv"
+    expected_table = Path("tests/expected_results/final_bins_quality_reports.tsv")
+    compare_results(result_table, expected_table)    
 
 
 @pytest.mark.requires_test_data
-def test_bin_tables_input(test_data_path: Path, tmp_path):
+def test_bin_tables_input_and_resume(test_data_path: Path, tmp_path):
 
     binning_results_dir = test_data_path / "binning_results"
 
@@ -102,9 +113,47 @@ def test_bin_tables_input(test_data_path: Path, tmp_path):
 
     result_table = tmp_path / "test_results_from_dirs" / "final_bins_quality_reports.tsv"
     expected_table = Path("tests/expected_results/final_bins_quality_reports.tsv")
-    content_actual = result_table.read_text()
-    content_expected = expected_table.read_text()
-    print(content_actual)
-    assert content_actual == content_expected, (
-    f"Content mismatch for {content_actual}. "
-    )
+    compare_results(result_table, expected_table)    
+
+    cmd_args.append("--resume")
+    result = runner.invoke(app, cmd_args)
+    print(result.output)
+    print(result.stderr)
+    assert result.exit_code == 0
+    
+
+
+
+@pytest.mark.requires_test_data
+def test_bin_tables_input_and_protein_input(test_data_path: Path, tmp_path):
+
+    binning_results_dir = test_data_path / "binning_results"
+
+    cmd_args = [
+        "-b",
+        binning_results_dir / "A.binning",
+        "-b",
+        binning_results_dir / "B.binning",
+        "-b",
+        binning_results_dir / "C.binning",
+        "--contigs",
+        test_data_path / "all_contigs.fna",
+        "--checkm2_db",
+        test_data_path / "checkm2_tiny_db/checkm2_tiny_db.dmnd",
+        "--proteins", 
+        test_data_path / "proteins.faa",
+        "-o",
+        tmp_path / "test_results_from_dirs",
+        "-v"
+    ]
+    cmd_args = [arg.as_posix() if isinstance(arg, Path) else arg for arg in cmd_args]
+    
+    result = runner.invoke(app, cmd_args)
+    print(result.output)
+    print(result.stderr)
+
+    assert result.exit_code == 0
+
+    result_table = tmp_path / "test_results_from_dirs" / "final_bins_quality_reports.tsv"
+    expected_table = Path("tests/expected_results/final_bins_quality_reports.tsv")
+    compare_results(result_table, expected_table)    
